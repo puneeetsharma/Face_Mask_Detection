cd599c1f5f7eee301fde1d9c87a72150169f71b1
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-04-20 19:18:40 +0530

    comments added

--- train_mask_Detector.py
+++ train_mask_Detector.py
@@ -94,7 +94,7 @@ for layer in baseModel.layers:
 # compile our model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt,#important
+model.compile(loss="binary_crossentropy", optimizer=opt,#important to learn
 	metrics=["accuracy"])
 
 # train the head of the network
