2dfc03e5bfccc114a44bd7930ae16f2ca988b23b
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-04-20 19:16:36 +0530

    comment added

--- train_mask_Detector.py
+++ train_mask_Detector.py
@@ -112,8 +112,7 @@ predIdxs = model.predict(testX, batch_size=BS)
 
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
-predIdxs = np.argmax(predIdxs, axis=1)#important
-
+predIdxs = np.argmax(predIdxs, axis=1)#important to learn
 # show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
